1d115f7f3d2429cee63d94dbe44d9c962e510c8b
Kridtakom <kridtakom.boom@mail.kmutt.ac.th>
2020-12-20 00:19:16 +0700

    [Add] home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./components/Home.js";
+import Navbar from "./components/Navbar.js";
+import Aboutme from "./components/Aboutme.js";
+import Contract from "./components/Contract.js";
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="App container">
+      <Navbar />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/aboutme">
+        <Aboutme />
+      </Route>
+      <Route path="/contract">
+        <Contract />
+      </Route>
     </div>
-  )
+  );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./TH.css";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="collapse navbar-collapse custom-navmenu" id="main-navbar">
+        <div className="container py-2 py-md-5">
+          <div className="row align-items-start">
+            <div className="col-md-12">
+              <ul className="custom-menu list-group list-group-horizontal">
+                <li className="navButton mx-3">
+                  <Link to="/"> Home</Link>
+                </li>
+                <li className="navButton mx-3">
+                  <Link to="/aboutme"> About Me </Link>
+                </li>
+                <li className="navButton mx-3">
+                  <Link to="/contract"> Contract </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav className="navbar navbar-light custom-navbar">
+        <div className="container">
+          <Link to="/" className="navbar-brand navBrand">
+            {"[{(MYPORTFOLIO)}]"}
+          </Link>
+
+          <a
+            href="#"
+            className="burger"
+            data-toggle="collapse"
+            data-target="#main-navbar"
+          >
+            <span></span>
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
